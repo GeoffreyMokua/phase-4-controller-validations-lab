10e974abc7025932f92c72c60ab25d94a3a0c575
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-11-24 21:38:32 +0300

    completed assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 
+ 
   def show
     post = Post.find(params[:id])
     
@@ -11,11 +12,13 @@ class PostsController < ApplicationController
 
     post.update(post_params)
 
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid 
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
-
   def post_params
     params.permit(:category, :content, :title)
   end
